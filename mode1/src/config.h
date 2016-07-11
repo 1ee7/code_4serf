0f6b8886666ab6799adfb92fed788212b7d912a7
1ee7 <h.w.fr33@gmail.com>
2016-07-11 09:57:07 +0800

    put the mode1 to  raspi

--- mode1/src/config.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-// profile--pathname, AppName--file content, KeyName--Key, KeyVal--val
-extern int GetProfileString(char *profile, char *AppName, char *KeyName, char *KeyVal )  ;
- 
-#endif
--- mode1/src/init.h
+++
@@ -1,8 +0,0 @@
-#ifndef INIT_H
-#define INIT_H
- int fd1; //fd for serial task1
- int udpsock;
-//char * target_ip;
-
-  extern void task2_init();
-#endif
